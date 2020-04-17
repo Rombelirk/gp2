a76efc5fc2f34d30048abc3bceedd40eb035dea2
Roman Beloborodov <rombelirk@gmail.com>
2020-04-17 14:52:39 +0300

    fix

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker build -t romanbeloborodov/gpclient:latest -t romanbeloborodov/gpclient:$SHA -f ./client/Dockerfile ./client
+docker build -t romanbeloborodov/gpserver:latest -t romanbeloborodov/gpserver:$SHA -f ./server/Dockerfile ./server
+
+docker push romanbeloborodov/gpclient:latest
+docker push romanbeloborodov/gpserver:latest
+
+docker push romanbeloborodov/gpclient:$SHA
+docker push romanbeloborodov/gpserver:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=romanbeloborodov/gpserver:$SHA
+kubectl set image deployments/client-deployment server=romanbeloborodov/gpclient:$SHA
